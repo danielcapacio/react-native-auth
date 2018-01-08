c6c7da0ef5c65ca3d195d3d211665785f38bd415
Daniel Capacio <danielcapacio@gmail.com>
2018-01-08 03:54:27 -0800

    Forgot an import

--- app/modules/auth/scenes/Welcome.js
+++ app/modules/auth/scenes/Welcome.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import { SocialIcon } from 'react-native-elements'
 
 import {Button} from 'react-native-elements'
 import {Actions} from 'react-native-router-flux'
